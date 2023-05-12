c50e3ddc6aecaed87c7ae1f3f4ce03d9189e41da
Jonathan Perret <j-github@jonathanperret.net>
2023-05-12 16:56:22 +0200

    Allow screen updates as binary websocket frames

--- src/websocket.cpp
+++ src/websocket.cpp
@@ -53,107 +53,114 @@ void onWsEvent(
   if (type == WS_EVT_DATA)
   {
     AwsFrameInfo *info = (AwsFrameInfo *)arg;
-    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+    if (info->final && info->index == 0 && info->len == len)
     {
-      data[len] = 0;
-
-      DynamicJsonDocument wsRequest(6144);
-      DeserializationError error = deserializeJson(wsRequest, data);
-
-      if (error)
-      {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.f_str());
-        return;
-      }
-      else
-      {
-        const char *event = wsRequest["event"];
-        if (!strcmp(event, "upload"))
-        {
-          if (currentMode != LOADING)
-          {
-            currentMode = LOADING;
-            int size = (int)wsRequest["screens"];
-
-            customAnimationFrames.resize(size);
-            for (int i = 0; i < size; i++)
-            {
-              for (int k = 0; k < 32; k++)
-              {
-                if (k == 0)
-                {
-                  customAnimationFrames[i].resize(32);
-                }
-                customAnimationFrames[i][k] = (int)wsRequest["data"][i][k];
-              }
-            }
-            setMode(CUSTOM, true);
-          }
-        }
-        else if (!strcmp(event, "mode"))
-        {
-          MODE mode = getModeByString(wsRequest["mode"]);
-          setModeByString(wsRequest["mode"], &sendModeToAllClients);
-
-          if (mode == NONE)
-          {
-            sendStateAndInfo();
-          }
-        }
-        else if (!strcmp(event, "persist-mode"))
-        {
-          persistMode();
-        }
-        else if (!strcmp(event, "rotate"))
-        {
-          bool isRight = (bool)!strcmp(wsRequest["direction"], "right");
-
-          Screen.currentRotation = isRight ? (Screen.currentRotation > 3 ? 1 : Screen.currentRotation + 1) : (Screen.currentRotation <= 0 ? 3 : Screen.currentRotation - 1);
-
+      if(info->opcode == WS_BINARY) {
+        if (info->len == 256) {
+          Screen.setRenderBuffer(data);
           Screen.render();
         }
-        else if (!strcmp(event, "info"))
-        {
-          sendStateAndInfo();
-        }
-        else if (!strcmp(event, "brightness"))
-        {
-          Screen.setBrightness(wsRequest["brightness"].as<unsigned int>());
-        }
+      } else if(info->opcode == WS_TEXT) {
+        data[len] = 0;
 
-        if (currentMode == NONE)
+        DynamicJsonDocument wsRequest(6144);
+        DeserializationError error = deserializeJson(wsRequest, data);
+
+        if (error)
         {
-          if (!strcmp(event, "clear"))
+          Serial.print(F("deserializeJson() failed: "));
+          Serial.println(error.f_str());
+          return;
+        }
+        else
+        {
+          const char *event = wsRequest["event"];
+          if (!strcmp(event, "upload"))
           {
-            Screen.clear();
-          }
-          else if (!strcmp(event, "led"))
-          {
-            Screen.setPixelAtIndex(wsRequest["index"], wsRequest["status"]);
-            Screen.render();
-          }
-          else if (!strcmp(event, "screen"))
-          {
-            uint8_t buffer[ROWS * COLS];
-            for (int i = 0; i < ROWS * COLS; i++)
+            if (currentMode != LOADING)
             {
-              buffer[i] = wsRequest["data"][i];
+              currentMode = LOADING;
+              int size = (int)wsRequest["screens"];
+
+              customAnimationFrames.resize(size);
+              for (int i = 0; i < size; i++)
+              {
+                for (int k = 0; k < 32; k++)
+                {
+                  if (k == 0)
+                  {
+                    customAnimationFrames[i].resize(32);
+                  }
+                  customAnimationFrames[i][k] = (int)wsRequest["data"][i][k];
+                }
+              }
+              setMode(CUSTOM, true);
             }
-            Screen.setRenderBuffer(buffer);
-            delay(10);
+          }
+          else if (!strcmp(event, "mode"))
+          {
+            MODE mode = getModeByString(wsRequest["mode"]);
+            setModeByString(wsRequest["mode"], &sendModeToAllClients);
+
+            if (mode == NONE)
+            {
+              sendStateAndInfo();
+            }
+          }
+          else if (!strcmp(event, "persist-mode"))
+          {
+            persistMode();
+          }
+          else if (!strcmp(event, "rotate"))
+          {
+            bool isRight = (bool)!strcmp(wsRequest["direction"], "right");
+
+            Screen.currentRotation = isRight ? (Screen.currentRotation > 3 ? 1 : Screen.currentRotation + 1) : (Screen.currentRotation <= 0 ? 3 : Screen.currentRotation - 1);
+
             Screen.render();
           }
-          else if (!strcmp(event, "persist"))
+          else if (!strcmp(event, "info"))
           {
-            Screen.persist();
-          }
-          else if (!strcmp(event, "load"))
-          {
-            Screen.loadFromStorage();
-            delay(10);
-            Screen.render();
             sendStateAndInfo();
+          }
+          else if (!strcmp(event, "brightness"))
+          {
+            Screen.setBrightness(wsRequest["brightness"].as<unsigned int>());
+          }
+
+          if (currentMode == NONE)
+          {
+            if (!strcmp(event, "clear"))
+            {
+              Screen.clear();
+            }
+            else if (!strcmp(event, "led"))
+            {
+              Screen.setPixelAtIndex(wsRequest["index"], wsRequest["status"]);
+              Screen.render();
+            }
+            else if (!strcmp(event, "screen"))
+            {
+              uint8_t buffer[ROWS * COLS];
+              for (int i = 0; i < ROWS * COLS; i++)
+              {
+                buffer[i] = wsRequest["data"][i];
+              }
+              Screen.setRenderBuffer(buffer);
+              delay(10);
+              Screen.render();
+            }
+            else if (!strcmp(event, "persist"))
+            {
+              Screen.persist();
+            }
+            else if (!strcmp(event, "load"))
+            {
+              Screen.loadFromStorage();
+              delay(10);
+              Screen.render();
+              sendStateAndInfo();
+            }
           }
         }
       }
